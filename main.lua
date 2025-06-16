9fe76eff0d5d7d295c568506ecffe3f45dca0e29
Alex Alex Desatoff <alexdesatoff2007@gmail.com>
2025-06-16 12:34:36 -0300

    C

--- main.lua
+++ main.lua
@@ -4,9 +4,9 @@ local Games = {
     [72907489978215] = "https://raw.githubusercontent.com/Skibidiking123/Fisch1/refs/heads/main/Sea2",
     [85896571713843] = "https://raw.githubusercontent.com/Skibidiking123/Fisch1/refs/heads/main/BGS",
     [122678592501168] = "https://raw.githubusercontent.com/Skibidiking123/Fisch1/refs/heads/main/Beaks",
-    [126884695634066] = "https://raw.githubusercontent.com/Skibidiking123/Fisch1/refs/heads/main/GAG",
-    [126884695634066] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/refs/heads/main/Grow%20a%20Garden.lua", 
-    [91867617264223] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/refs/heads/main/Grow%20a%20Garden.lua",
+    [126884695634066] = "https://raw.githubusercontent.com/Desatoff/Scripts/main/GAG.lua",
+    [126884695634066] = "https://raw.githubusercontent.com/Desatoff/Scripts/main/GrowAGarden.lua", 
+    [91867617264223] = "https://raw.githubusercontent.com/Desatoff/Scripts/main/GrowAGarden.lua",
 }
 
 for PlaceID, Execute in pairs(Games) do
